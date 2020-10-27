d4e5de67d0b8fff2d3143d1cc1344a19093effcf
Conner McCabe <conner.p.mccabe@gmail.com>
2020-10-27 12:02:48 -0500

    connected to alan and functional

--- src/App/App.js
+++ src/App/App.js
@@ -1,13 +1,16 @@
 import React, { useEffect } from 'react';
 import alanBtn from '@alan-ai/alan-sdk-web'
 import './App.css';
+
 import { NewsCards, Modal } from '../Component'
+
+const alanKey = process.env.REACT_APP_ALAN_KEY;
 
 
 const App = () => {
   useEffect(() => {
     alanBtn({
-
+      key: alanKey,
     })
   }, [])
 
